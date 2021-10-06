0f62902944b1ee146938fab09910db2aa9ac0db5
mcuellar.supervisa@gmail.com <macsdevelop008@gmail.com>
2021-10-06 10:37:29 -0500

    Base proyecto web - ApacheNetBeans

--- buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.dinamitaPower/Taller_2_SOAP .
-call docker rm -f Taller_2_SOAP
-call docker run -d -p 9080:9080 -p 9443:9443 --name Taller_2_SOAP com.dinamitaPower/Taller_2_SOAP
